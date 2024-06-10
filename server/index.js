acdeba1f7db5c76e29b5f91b4ff8c636868e05b5
Facundo Fabian Duette <47751520+WestMorris@users.noreply.github.com>
2024-06-09 21:27:05 -0400

    funcional
    ok

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 require('./config/mongoose.config');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000; 
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 require('./routes/product.routes')(app);
 
-app.listen(8000, () => {
-    console.log(`Listening at Port ${8000}`)
+app.listen(process.env.PORT, () => {
+    console.log("lISTO")
 });
